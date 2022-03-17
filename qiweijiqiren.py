9a248a4ae8a509f51557c18a7f765f1bb7f85fab
xiaokuangfeng <101633886+xiaokuangfeng@users.noreply.github.com>
2022-03-17 17:56:20 +0800

    Add files via upload

---
+++ qiweijiqiren.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import json
+
+# 通知信息（企微群）
+def test_text():
+    # Webhook地址
+    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + "333f3b50-4cdc-4998-a916-243da9e2e96f"
+    HEADERS = {
+        "Content-Type": "application/json ;charset=utf-8"
+    }
+    # 格式为：text
+    message = "质量报告填写通知："
+    String_textMsg = {
+        "msgtype": "text",
+        "text": {"content": message + "\n\n"
+                 "同学们，今天周五咯，记得写质量报告！" + "\n"
+                 "同学们，今天周五咯，记得写质量报告！" + "\n"
+                 "同学们，今天周五咯，记得写质量报告！" + "\n\n"
+                 "(*＾-＾*)" + "\n"
+                 }
+    }
+    String_textMsg = json.dumps(String_textMsg)
+    requests.packages.urllib3.disable_warnings()
+    res = requests.post(url, data=String_textMsg, headers=HEADERS, verify=False)
+    print(res.text)
+
+if __name__=="__main__":
+    test_text()
