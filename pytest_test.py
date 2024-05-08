127120bb43c0c35e4d80146d407ca35c24d7b323
xiaokuangfeng <101633886+xiaokuangfeng@users.noreply.github.com>
2024-05-08 17:42:14 +0800

    Update pytest_test.py
    edition

--- pytest_test.py
+++ pytest_test.py
@@ -4,22 +4,21 @@
 # File : pytest_test.py
 # @Software : PyCharm
 
-from utils import get_text_data, get_csv_data, get_excle_data
 import pytest
 import time
+import utils
+from utils import get_text_data, get_csv_data, get_excle_data, get_json_data, get_file_content
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
-
 # 传统pytest.mark.parameter方法实现参数化，嵌套列表
 @pytest.mark.parametrize(["username1", "password1"], [[456, 111], [123, 456], ["i", "o"], ["wa", "heihei"]])
 def test_a(username1, password1):
     print(f"输入用户名:{username1}")  # f为字符串转义
     print(f"输入密码:{password1}")
-
 
 # 调用utils包里面的get_text_data()封装方法
 # 返回txt文档中的字符串参数化值：去掉\n换行符和空格，以,逗号进行分割
@@ -28,7 +27,6 @@ def test_b(username2, password2):
     print(f"输入用户名:{username2}")
     print(f"输入密码:{password2}")
 
-
 # 调用utils包里面的get_csv_data()封装方法
 @pytest.mark.parametrize(["username3", "password3", "code3"], get_csv_data())
 def test_c(username3, password3, code3):
@@ -36,20 +34,45 @@ def test_c(username3, password3, code3):
     print(f"输入密码:{password3}")
     print(f"输入验证码:{code3}")
 
-
 # pytest实战：根据csv文档，实现参数化验证登录功能
 @pytest.mark.parametrize(["username4", "password4", "code4"], get_csv_data())
 def test_d(username4, password4, code4):
-    driver = webdriver.Chrome()
-    driver.get("http://192.168.5.213:5555/api-a/index")
-    driver.maximize_window()
-    time.sleep(2)
-    driver.find_element(By.NAME, "loginId").send_keys(username4)
-    driver.find_element(By.NAME, "pwd").send_keys(password4)
-    time.sleep(2)
-    driver.find_element(By.CSS_SELECTOR, "#loginform > div.__3wh-Hw9P.mt10 > input").click()
-    driver.quit()
+    # #简易案例登录系统，输入账号密码
+    # driver = webdriver.Chrome()
+    # driver.maximize_window()
+    # driver.get("http://192.168.5.213:5555")
+    # time.sleep(1)
+    # driver.find_element(By.NAME, "loginId").send_keys(username4)
+    # driver.find_element(By.NAME, "pwd").send_keys(password4)
+    # time.sleep(1)
+    # driver.find_element(By.CSS_SELECTOR, "#loginform > div.__3wh-Hw9P.mt10 > input").click()
+    # driver.quit()
 
+    # #增强用例，调用验证码识别组件，实现用户名、密码、验证码识别的登录测试
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.jiugh.com.cn:15566/d?m=frame&p=1686735468341")
+    time.sleep(1)
+    # 输入用户名
+    driver.find_element(By.XPATH, "//div[@class='login-username wr100 pr ']//input").send_keys(username4)
+    # 输入密码
+    driver.find_element(By.XPATH, "//div[@class='login-password wr100 pr ']//input").send_keys(password4)
+    time.sleep(1)
+    # 对界面进行截图
+    driver.find_element(By.XPATH, "//div[@class='check-center wr75 ']//img").screenshot(
+        'D:\python3\project\yanzhengma_shibie\img\yanzhengma.png'
+    )
+    time.sleep(1)
+    # 调用工具包utils.get_file_content方法，输入实时验证码
+    driver.find_element(By.XPATH, "//div[@class='check-center wr75 ']//input").send_keys(
+        utils.get_file_content()
+    )
+    # 点击登录
+    driver.find_element(By.XPATH, "//span[@class='fs16 colorw pac ffss ']").click()
+    time.sleep(2)
+    element = driver.find_element(By.XPATH, '//div[text()="你好,"]')
+    assert element.is_displayed()
+    driver.quit()
 
 # pytest实战：根据excle文档，实现参数化验证登录功能
 @pytest.mark.parametrize(["username5", "password5"], get_excle_data())
@@ -60,16 +83,13 @@ def test_e(username5, password5):
     driver.get(url)
     driver.maximize_window()
     time.sleep(1)
-
     # 输入用户名和密码
     print(f"输入用户名：{username5}")
     driver.find_element(By.NAME, "loginId").send_keys(username5)
     print(f"输入密码：{password5}")
     driver.find_element(By.NAME, "pwd").send_keys(password5)
-
     # 执行登录操作登录
     driver.find_element(By.CSS_SELECTOR, "#loginform > div.__3wh-Hw9P.mt10 > input").click()
-
     try:
         # 登录限制，是否弹窗
         alert_button = driver.find_element(By.XPATH, "//button[text()='确定']")
@@ -102,25 +122,19 @@ def test_e(username5, password5):
     assert driver.title == "联通物联网数字化运营平台"
     print("判断标题：" + driver.title + "，是否为：联通物联网数字化运营平台")
     time.sleep(1)
-
     driver.find_element(By.XPATH, "//input[@id='search_lst']").send_keys("N1N2统计分析")
     time.sleep(1)
-
     driver.find_element(By.XPATH, "//span[@class='aa-suggestions']//div[@class='aa-suggestion']").click()
     print("点击进入搜索框下拉菜单中的模块")
     time.sleep(1)
-
     driver.switch_to.frame(1)
     print("切换iframe")
     time.sleep(1)
-
     driver.find_element(By.XPATH, "//div[@id='queryid']").click()
     print("执行查询操作")
     time.sleep(1)
-
     print("默认120秒，等待元素消失")
     WebDriverWait(driver, 120).until(
         EC.invisibility_of_element_located((By.XPATH, "//div[contains(@class,'mask-spinner')]"))
     )
-
     driver.quit()
